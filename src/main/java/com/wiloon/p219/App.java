6abd7f64a09d0be4b86528bece8b03d3f6e854d5
wiloon <wiloon.wy@gmail.com>
2022-01-18 21:39:15 +0800

    spring boot restful API OK

---
+++ src/main/java/com/wiloon/p219/App.java
@@ -0,0 +1,14 @@
+package com.wiloon.p219;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 无限层级留言
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
