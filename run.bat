05f97953a876bffe746b2ad1bbe5d7cc28fb341a
wiloon <wiloon.wy@gmail.com>
2022-01-25 05:39:37 +0800

    pom版本修改, comments-tree:1.0.0

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 
-mvn clean package -Dmaven.test.skip=true && java -jar comments-tree-api/target/comments-tree-api-1.0.0-SNAPSHOT.jar
+mvn clean package -Dmaven.test.skip=true && java -jar comments-tree-api/target/comments-tree-api-1.0.0.jar
